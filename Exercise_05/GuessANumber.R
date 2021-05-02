d0bc5ae1a0f80fab4d6ab4df4a118b7629e78010
Salim Oussayfi <box@slimou.net>
2021-05-02 15:07:27 +0200

    first working exercise in R :)

--- Exercise_05/GuessANumber.R
+++ Exercise_05/GuessANumber.R
@@ -1,5 +1,32 @@
-yourNumber <- readline("guess a number: ")
+#random Number between 1 and 100
+randomNumber <- as.integer(runif(1, min = 1, max = 100))
 
-computersNumber <- runif(1, min=1, max=100)
+#get input from console and send to checkNumber
+getInputFromConsole <- function()
+{
+  checkNumber(as.integer(readline("Number between 1 and 100:")))
+}
 
-as.integer(computersNumber)
+#check the given number is searched number
+checkNumber <- function(n) {
+  if (n != randomNumber)
+  {
+    if (n < randomNumber)
+    {
+      cat("Searched number bigger!\n")
+    }
+    else if (n > randomNumber)
+    {
+      cat("Searched number smaller!\n")
+    }
+    getInputFromConsole()
+  }
+  else if (as.integer(n == randomNumber))
+    {
+      cat("You must be a Nostradamus!", randomNumber, "is the searched number.\n")
+    }
+}
+
+getInputFromConsole()
+
+
